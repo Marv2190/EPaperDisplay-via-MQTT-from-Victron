a57bdcce6d1cbdd0cc4858c4907ef24ac3735015
Marv2190 <jabber@onlinehome.de>
2021-11-08 02:47:25 +0100

    Added two "Exceptionscatches"
    Sometimes the Script crashed, but i still dont know why. Maybe something with the Display Libary or the MQTT Story in this Script :D

--- Display.py
+++ Display.py
@@ -30,6 +30,7 @@ hausverbrauch =1
 akkuladen=1
 akkuspg=1
 zaehler=0
+num=0
 neuaufbau=2000
 
 logging.basicConfig(level=logging.DEBUG)
@@ -163,35 +164,42 @@ try:
     epd.Clear(0xFF)
 
     while (True):
-        if(neuaufbau == 2000):# Wenn 2000 dann wird komplettes Bild neu aufgebaut
-            epd.Clear(0xFF)
-            time_image = Image.new('1', (epd.height, epd.width), 0)
-            time_image = Image.open(os.path.join(picdir, 'bildinv.bmp'))
-            time_draw = ImageDraw.Draw(time_image)
-            epd.display(epd.getbuffer(time_image))
-            neuaufbau = 0
-        time_draw.rectangle((130, 0, 220, 60), fill = 0) # Mal alles wieder weiß wo aktualisiert wird,Rechts
-        time_draw.rectangle((35, 0, 95, 60), fill = 0) # Mal alles wieder weiß wo aktualisiert wird, Links
-        time_draw.rectangle((29, 80, 83, 104), fill = 0) # Mal alles wieder weiß wo aktualisiert wird, Grid
-        time_draw.rectangle((89, 80, 115, 97), fill = 0) # Mal alles wieder weiß wo aktualisiert wird, Haus
-        time_draw.rectangle((121, 80, 173, 104), fill = 0) # Mal alles wieder weiß wo aktualisiert wird, Akku
-        time_draw.rectangle((176, 81, 203, 97), fill = 0) # Mal alles wieder weiß wo aktualisiert wird, Akkuspg
-        time_draw.text((140, 5), time.strftime('%H:%M:%S'), font = font15, fill = 255) # Uhrzeit
-        time_draw.text((33, 9), str(pvgesamt)+"W", font = font17, fill = 255) # pvgesamt
-        time_draw.text((127, 20), str(se)+"kWh", font = font17, fill = 255) #se
-        time_draw.text((127, 36), str(ve)+"kWh", font = font17, fill = 255) #ve
-        time_draw.text((35, 40), str(("%.4f" % akku)[:4])+"%", font = font17, fill = 255) #akkuprozent
-        time_draw.text((89, 82), str(("%.4f" % hausverbrauch)[:4]).rstrip('0').rstrip('.'), font = font15, fill = 255) #hausverbrauch
-        time_draw.text((35, 82), str(("%.4f" % grid)[:6]).rstrip('0').rstrip('.'), font = font17, fill = 255) #Grid
-        time_draw.text((125, 82), str(("%.4f" % akkuladen)[:6]).rstrip('0').rstrip('.'), font = font17, fill = 255) #akkuladen
-        time_draw.text((176, 84), str(("%.4f" % akkuspg)[:4])+"V", font = font12, fill = 255) #akkuspg
-        # newimage = time_image.crop([10, 10, 120, 50])
-        # time_image.paste(newimage, (50,30))
-        epd.DisplayPartial(epd.getbuffer(time_image))
-        neuaufbau = neuaufbau + 1
-        neuaufbauanzeige = 2000-neuaufbau
-        print("Neuaufbau  " + str(neuaufbauanzeige))
-        time.sleep(30)
+        try:
+            if(neuaufbau == 2000):# Wenn 2000 dann wird komplettes Bild neu aufgebaut
+                epd.Clear(0xFF)
+                time_image = Image.new('1', (epd.height, epd.width), 0)
+                time_image = Image.open(os.path.join(picdir, 'bildinv.bmp'))
+                time_draw = ImageDraw.Draw(time_image)
+                epd.display(epd.getbuffer(time_image))
+                neuaufbau = 0
+            time_draw.rectangle((130, 0, 220, 60), fill = 0) # Mal alles wieder weiß wo aktualisiert wird,Rechts
+            time_draw.rectangle((35, 0, 95, 60), fill = 0) # Mal alles wieder weiß wo aktualisiert wird, Links
+            time_draw.rectangle((29, 80, 83, 104), fill = 0) # Mal alles wieder weiß wo aktualisiert wird, Grid
+            time_draw.rectangle((89, 80, 115, 97), fill = 0) # Mal alles wieder weiß wo aktualisiert wird, Haus
+            time_draw.rectangle((121, 80, 173, 104), fill = 0) # Mal alles wieder weiß wo aktualisiert wird, Akku
+            time_draw.rectangle((176, 81, 203, 97), fill = 0) # Mal alles wieder weiß wo aktualisiert wird, Akkuspg
+            time_draw.text((140, 5), time.strftime('%H:%M:%S'), font = font15, fill = 255) # Uhrzeit
+            time_draw.text((33, 9), str(pvgesamt)+"W", font = font17, fill = 255) # pvgesamt
+            time_draw.text((127, 20), str(se)+"kWh", font = font17, fill = 255) #se
+            time_draw.text((127, 36), str(ve)+"kWh", font = font17, fill = 255) #ve
+            time_draw.text((35, 40), str(("%.4f" % akku)[:4])+"%", font = font17, fill = 255) #akkuprozent
+            time_draw.text((89, 82), str(("%.4f" % hausverbrauch)[:4]).rstrip('0').rstrip('.'), font = font15, fill = 255) #hausverbrauch
+            time_draw.text((35, 82), str(("%.4f" % grid)[:6]).rstrip('0').rstrip('.'), font = font17, fill = 255) #Grid
+            time_draw.text((125, 82), str(("%.4f" % akkuladen)[:6]).rstrip('0').rstrip('.'), font = font17, fill = 255) #akkuladen
+            time_draw.text((176, 84), str(("%.4f" % akkuspg)[:4])+"V", font = font12, fill = 255) #akkuspg
+            # newimage = time_image.crop([10, 10, 120, 50])
+            # time_image.paste(newimage, (50,30))
+            epd.DisplayPartial(epd.getbuffer(time_image))
+            neuaufbau = neuaufbau + 1
+            neuaufbauanzeige = 2000-neuaufbau
+            print("Neuaufbau  " + str(neuaufbauanzeige))
+            time.sleep(30)
+        except KeyboardInterrupt:
+            print("STRG+C erkannt - beende")
+            break
+        except:
+            print("Irgendwas ist in der Displaywhileschleife schief gelaufen ;( ")
+
 
 
     client.loop_stop()
@@ -200,3 +208,6 @@ try:
 except KeyboardInterrupt:
     print("\ninterrupted!")
     client.loop_stop()
+
+except:
+    print("Hier ist was schief gelaufen")
